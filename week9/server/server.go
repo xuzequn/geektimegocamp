b4404bbfe675ff58f9ce01a8e844cf06e6d72183
xuzequn <xzqyymt@126.com>
2021-07-03 10:42:39 +0800

    week9-02

--- week9/server/server.go
+++ week9/server/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"io"
 	"log"
 	"net"
@@ -22,10 +24,15 @@ func start() {
 		}
 		defer conn.Close()
 
+		// 创建一个bufio.Reader 实体 接收数据
 		reader := bufio.NewReader(conn)
 		for {
+			// 创建一个接收数据的切片数组
 			// var data = make([]byte, 5)
-			data, err := reader.ReadSlice('\n')
+			//  通过ReaderSlice方法 以“\n” 进行切分数据
+			// data, err := reader.ReadSlice('\n')
+			// 通过peek 去前几个字节的数据返回一个[]byte
+			peek, err := reader.Peek(4)
 
 			// n, err := conn.Read(data)
 			if err != nil {
@@ -35,7 +42,28 @@ func start() {
 					break
 				}
 			}
-			log.Println("received msg", len(data), "bytes:", string(data))
+
+			//  通过返回的[]byte 创建一个byte类型的buffer
+			buffer := bytes.NewBuffer(peek)
+			var length int32
+			// 字节缓存 转化成 数字 ， binary.BigEndian 转化规则方法集。 三个参数分别是 转换前的字符数据， 转换方法集， 转换成的数据
+			err = binary.Read(buffer, binary.BigEndian, &length)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if int32(reader.Buffered()) < length+4 {
+				continue
+			}
+			data := make([]byte, length+4)
+			_, err = reader.Read(data)
+			if err != nil {
+				continue
+			}
+
+			log.Println("received msg", string(data[:4]))
+
+			// log.Println("received msg", len(data), "bytes:", string(data))
 
 			// if n > 0 {
 			// log.Println("received msg", n, "bytes:", string(data[:n]))
